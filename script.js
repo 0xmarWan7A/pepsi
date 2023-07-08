36c08d58ce28c2dafdc57d54ad2dce413af81343
Marwan 7assaAn <69312897+0xmarWan7A@users.noreply.github.com>
2023-07-08 16:32:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function product(anything) {
+    document.querySelector(".product").src = anything;
+}
+
+function changeColor(color) {
+    
+    const body = document.querySelector("body");
+    body.style.background = color
+
+}
